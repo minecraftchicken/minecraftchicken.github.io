11e3012b6dadf19a761820d1aeb7ac9d4076b108
Grabstertv <49114212+Grabstertv@users.noreply.github.com>
2020-10-05 00:15:38 +0500

    Header is now fixed to the page + minor fixes

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 
 function myFunction(x) {
     x.classList.toggle("change");
-  }
+}
 
 
 /* Custom Cursor */
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: "+(e.pageY - 5)+"px; left: "+(e.pageX - 10)+"px;")
+    cursor.setAttribute("style", "top: " + (e.pageY - 5) + "px; left: " + (e.pageX - 10) + "px;")
 })
 
 document.addEventListener('click', () => {
@@ -24,73 +24,73 @@ document.addEventListener('click', () => {
 
 
 /* Smooth mousehweel scroll */
-function init(){
-	new SmoothScroll(document,120,12)
+function init() {
+    new SmoothScroll(document, 120, 12)
 }
 
 function SmoothScroll(target, speed, smooth) {
-	if (target === document)
-		target = (document.scrollingElement 
-              || document.documentElement 
-              || document.body.parentNode 
-              || document.body) // cross browser support for document scrolling
-      
-	var moving = false
-	var pos = target.scrollTop
-  var frame = target === document.body 
-              && document.documentElement 
-              ? document.documentElement 
-              : target // safari is the new IE
-  
-	target.addEventListener('mousewheel', scrolled, { passive: false })
-	target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
+    if (target === document)
+        target = (document.scrollingElement
+            || document.documentElement
+            || document.body.parentNode
+            || document.body) // cross browser support for document scrolling
 
-	function scrolled(e) {
-		e.preventDefault(); // disable default scrolling
+    var moving = false
+    var pos = target.scrollTop
+    var frame = target === document.body
+        && document.documentElement
+        ? document.documentElement
+        : target // safari is the new IE
 
-		var delta = normalizeWheelDelta(e)
+    target.addEventListener('mousewheel', scrolled, { passive: false })
+    target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
 
-		pos += -delta * speed
-		pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
+    function scrolled(e) {
+        e.preventDefault(); // disable default scrolling
 
-		if (!moving) update()
-	}
+        var delta = normalizeWheelDelta(e)
 
-	function normalizeWheelDelta(e){
-		if(e.detail){
-			if(e.wheelDelta)
-				return e.wheelDelta/e.detail/40 * (e.detail>0 ? 1 : -1) // Opera
-			else
-				return -e.detail/3 // Firefox
-		}else
-			return e.wheelDelta/120 // IE,Safari,Chrome
-	}
+        pos += -delta * speed
+        pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
 
-	function update() {
-		moving = true
-    
-		var delta = (pos - target.scrollTop) / smooth
-    
-		target.scrollTop += delta
-    
-		if (Math.abs(delta) > 0.5)
-			requestFrame(update)
-		else
-			moving = false
-	}
+        if (!moving) update()
+    }
 
-	var requestFrame = function() { // requestAnimationFrame cross browser
-		return (
-			window.requestAnimationFrame ||
-			window.webkitRequestAnimationFrame ||
-			window.mozRequestAnimationFrame ||
-			window.oRequestAnimationFrame ||
-			window.msRequestAnimationFrame ||
-			function(func) {
-				window.setTimeout(func, 1000 / 100);
-			}
-		);
-	}()
+    function normalizeWheelDelta(e) {
+        if (e.detail) {
+            if (e.wheelDelta)
+                return e.wheelDelta / e.detail / 40 * (e.detail > 0 ? 1 : -1) // Opera
+            else
+                return -e.detail / 3 // Firefox
+        } else
+            return e.wheelDelta / 120 // IE,Safari,Chrome
+    }
+
+    function update() {
+        moving = true
+
+        var delta = (pos - target.scrollTop) / smooth
+
+        target.scrollTop += delta
+
+        if (Math.abs(delta) > 0.5)
+            requestFrame(update)
+        else
+            moving = false
+    }
+
+    var requestFrame = function () { // requestAnimationFrame cross browser
+        return (
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function (func) {
+                window.setTimeout(func, 1000 / 100);
+            }
+        );
+    }()
 }
 
 
