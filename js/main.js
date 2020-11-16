c9e54c3d9e161bb4cf149ee183cc63ff4c0c49f5
Grabstertv <49114212+Grabstertv@users.noreply.github.com>
2020-11-17 00:17:03 +0500

    5.0
    - New imagery
    - Fixes
    - Abstract page full screen view
    - 404

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ function myFunction(x) {
     x.classList.toggle("change");
 }
 
+function fullScreen(x) {
+    x.classList.toggle("change");
+}
+
+
 
 /* Custom Cursor 
 const cursor = document.querySelector('.cursor');
@@ -12,8 +17,8 @@ document.addEventListener('mousemove', e => {
     cursor.setAttribute("style", "top: "+(e.pageY - 5)+"px; left: "+(e.pageX - 10)+"px;")
 }) */
 
-/* Add class to cursor on hover */
-$(document).ready(function() {  
+/* Add class to cursor on hover 
+$(document).ready(function() {  */
     /* Classes & IDs that needs to be hovered  
         $('#hamburger-hover, #menu-item-hover, .social-icon, .logo-image, .flex-item-header').hover(function(){      
             $('#cursor-hover').addClass('cursor-hover-active');
@@ -24,77 +29,22 @@ $(document).ready(function() {
     });
 */  
 
-/* Smooth mousehweel scroll 
-function init() {
-    new SmoothScroll(document, 120, 12)
-}
+/* Copy to clipoard */
 
-function SmoothScroll(target, speed, smooth) {
-    if (target === document)
-        target = (document.scrollingElement
-            || document.documentElement
-            || document.body.parentNode
-            || document.body) // cross browser support for document scrolling
-
-    var moving = false
-    var pos = target.scrollTop
-    var frame = target === document.body
-        && document.documentElement
-        ? document.documentElement
-        : target // safari is the new IE
-
-    target.addEventListener('mousewheel', scrolled, { passive: false })
-    target.addEventListener('DOMMouseScroll', scrolled, { passive: false })
-
-    function scrolled(e) {
-        e.preventDefault(); // disable default scrolling
-
-        var delta = normalizeWheelDelta(e)
-
-        pos += -delta * speed
-        pos = Math.max(0, Math.min(pos, target.scrollHeight - frame.clientHeight)) // limit scrolling
-
-        if (!moving) update()
-    }
-
-    function normalizeWheelDelta(e) {
-        if (e.detail) {
-            if (e.wheelDelta)
-                return e.wheelDelta / e.detail / 40 * (e.detail > 0 ? 1 : -1) // Opera
-            else
-                return -e.detail / 3 // Firefox
-        } else
-            return e.wheelDelta / 120 // IE,Safari,Chrome
-    }
-
-    function update() {
-        moving = true
-
-        var delta = (pos - target.scrollTop) / smooth
-
-        target.scrollTop += delta
-
-        if (Math.abs(delta) > 0.5)
-            requestFrame(update)
-        else
-            moving = false
-    }
-
-    var requestFrame = function () { // requestAnimationFrame cross browser
-        return (
-            window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function (func) {
-                window.setTimeout(func, 1000 / 100);
-            }
-        );
-    }()
-}
-*/
-
+function myFunction() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+    
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copied: " + copyText.value;
+  }
+  
+  function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+  }
 
 
 
