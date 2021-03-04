3bef13f32a551da89e68f8cb266a3504e67e03c1
Grabstertv <49114212+Grabstertv@users.noreply.github.com>
2021-03-05 02:55:07 +0500

    8.6 - Contact form
    - Added contact form for testing
    - Other slight changes

---
+++ php/contactform.php
@@ -0,0 +1,14 @@
+<?php
+
+if (isset($POST ['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "contact@gabsterstudios.com"
+    $headers = "From: ".$mailFrom;
+    $txt = "New message from website from ".$name."./n/n".$message;
+
+    mail($mailTo, $txt, $headers);
+    header("Location: index.php?mailsend")
+}
